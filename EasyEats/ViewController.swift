7751c62cd8195203be327d290dfb1b3a8cd51347
Jung <exodus0917>
2017-11-16 13:25:20 -0600

    process

--- EasyEats/ViewController.swift
+++ EasyEats/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceSlider: UISlider!
+    
+    
+    @IBOutlet weak var timeSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
