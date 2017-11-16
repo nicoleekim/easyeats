07718d6eba7510179cea5b141543a0a8f764145b
Jung <exodus0917>
2017-11-16 14:26:33 -0600

    passing values

--- EasyEats/ViewController.swift
+++ EasyEats/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var priceSlider: UISlider!
     
+    @IBOutlet weak var priceOutlet: UILabel!
     
     @IBOutlet weak var timeSlider: UISlider!
     
+    @IBOutlet weak var timeOutlet: UILabel!
+    
+    // Shows a text price value of slider to user
+    @IBAction func priceChange(_ sender: Any) {
+        priceOutlet.text = "Max Price: " + "$\(Int(priceSlider.value))"
+    }
+    
+    // Shows a text time value of slider to user
+    @IBAction func timeChange(_ sender: Any) {
+        timeOutlet.text = "Max Time: " + "\(Int(timeSlider.value)) min"
+    }
+    
+    
+    @IBAction func searchButton(_ sender: Any) {
+        performSegue(withIdentifier: "segue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let filterSearchController = segue.destination as! FilteredSearch_ViewController
+        filterSearchController.maxPrice = Int(priceSlider.value)
+        filterSearchController.maxTime = Int(timeSlider.value)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
