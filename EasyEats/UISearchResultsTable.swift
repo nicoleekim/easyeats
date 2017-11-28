53079c59cdb47a2328f26fcc86377847ecd29a2c
nicolekim <nicole.e.kim@vanderbilt.edu>
2017-11-28 11:19:25 -0600

    Added custom tableview class

---
+++ EasyEats/UISearchResultsTable.swift
@@ -0,0 +1,21 @@
+//
+//  UISearchResultsTable.swift
+//  EasyEats
+//
+//  Created by Nicole Kim on 11/28/17.
+//  Copyright © 2017 Easy Eats. All rights reserved.
+//
+
+import UIKit
+
+class UISearchResultsTable: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
