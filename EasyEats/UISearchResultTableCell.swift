a521032e38bd5bdcf9792408d02f532ecebc2cc7
nicolekim <nicole.e.kim@vanderbilt.edu>
2017-11-28 11:44:40 -0600

    Starting table stuff

---
+++ EasyEats/UISearchResultTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  UISearchResultTableCell.swift
+//  EasyEats
+//
+//  Created by Nicole Kim on 11/28/17.
+//  Copyright © 2017 Easy Eats. All rights reserved.
+//
+
+import UIKit
+
+class UISearchResultTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
