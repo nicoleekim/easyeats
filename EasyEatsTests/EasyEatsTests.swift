b1cfe4d9f1b623d7f111489766d354b4942e4fcd
Cathy Lin <cathylin@Cathys-MacBook-Pro.local>
2017-12-08 02:25:28 -0600

    started writing unit tests and building structures for testing

--- EasyEatsTests/EasyEatsTests.swift
+++ EasyEatsTests/EasyEatsTests.swift
@@ -11,20 +11,74 @@ import XCTest
 
 class EasyEatsTests: XCTestCase {
     
+    var recipeHolder = [Recipe]() //need a container to run filters on
+
+    var veganDish:Recipe! //clear labels for purpose of each dish
+    var vegetDish:Recipe!
+    var cheapDish:Recipe!
+    var richDish:Recipe!
+    var shortDish:Recipe!
+    var longDish:Recipe!
+    var filter:FilteredSearch_ViewController!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        //only vegan dish is veganDish
+        //only vegetarian dishes are veganDish and vegetDish
+        //Rank of prices: cheapDish (0.50), veganDish + longDish (2.00), vegetDish + shortDish (5.00), richDish (10.00)
+        //Rank of prepTime: shortDish (15), veganDish + vegetDish + richDish (20), cheapDish (30), longDish (45)
+        
+        veganDish = Recipe(name: "Vegan Dish", vegetarian: true, vegan: true, prepTime: 20, servings: 1, price: 2.00, ingredients: ["a"], instructions: ["a"], photoURL: "www.somesite.png")
+        
+        vegetDish = Recipe(name: "Veget Dish", vegetarian: true, vegan: false, prepTime: 20, servings: 1, price: 5.00, ingredients: ["a"], instructions: ["a"], photoURL: "www.somesite.png")
+        
+        cheapDish = Recipe(name: "Cheap Dish", vegetarian: false, vegan: false, prepTime: 30, servings: 1, price: 0.50, ingredients: ["a"], instructions: ["a"], photoURL: "www.somesite.png")
+        
+        richDish = Recipe(name: "Rich Dish", vegetarian: false, vegan: false, prepTime: 20, servings: 1, price: 10.00, ingredients: ["a"], instructions: ["a"], photoURL: "www.somesite.png")
+        
+        shortDish = Recipe(name: "Short Dish", vegetarian: false, vegan: false, prepTime: 15, servings: 1, price: 5.00, ingredients: ["a"], instructions: ["a"], photoURL: "www.somesite.png")
+
+        longDish = Recipe(name: "Long Dish", vegetarian: false, vegan: false, prepTime: 45, servings: 1, price: 2.00, ingredients: ["a"], instructions: ["a"], photoURL: "www.somesite.png")
+        
+        recipeHolder = [veganDish, vegetDish, cheapDish, richDish, shortDish, longDish]
+ 
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        veganDish = nil
+        vegetDish = nil
+        cheapDish = nil
+        richDish = nil
+        shortDish = nil
+        longDish = nil
+        recipeHolder = []
+        filter = nil
     }
     
-    func testExample() {
+    func testVeganFilter() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
+        filter?.isVegan = true //looking for vegan recipes
+        filter?.isVegetarian = true
+        filter?.maxPrice = 50
+        filter?.maxTime = 60
+        var data = [Recipe]()
+        for recipe in recipeHolder {
+            if filter!.simpleFilter(price: recipe.price, time: recipe.prepTime, vegan: recipe.vegan, vegetarian: recipe.vegetarian) {
+                data.append(recipe)
+            }
+        }
+        XCTAssertTrue(data.count == 1) //only one recipe was vegan
+        
     }
+ 
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
